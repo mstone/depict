2fc63ce825269f32471c74a6a1419ff8f74b9971
Michael Stone <m@mstone.info>
2022-01-20 22:21:00 -0500

    tikz: calculate layers

--- src/tikz.rs
+++ src/tikz.rs
@@ -1,6 +1,7 @@
 use diagrams::parser::{Ident, parse};
 use diagrams::render::{Fact, Syn, filter_fact, resolve, unwrap_atom, find_parent, to_ident, as_string};
-use std::collections::HashMap;
+use petgraph::EdgeDirection::Incoming;
+use std::collections::{HashMap, BTreeMap};
 use std::collections::hash_map::{Entry};
 use std::fs::read_to_string;
 use std::env::args;
@@ -8,8 +9,10 @@ use std::io;
 use std::process::exit;
 use nom::error::convert_error;
 use indoc::indoc;
-use petgraph::graph::{Graph, DefaultIx, NodeIndex};
+use petgraph::graph::{Graph, DefaultIx, NodeIndex, EdgeReference};
 use petgraph::dot::{Dot};
+use petgraph::visit::{NodeFiltered};
+use petgraph::algo::floyd_warshall::floyd_warshall;
 
 pub fn tikz_escape(s: &str) -> String {
     s
@@ -23,9 +26,15 @@ pub fn or_insert<'a, 'g, 'h>(g: &'g mut Graph<&'a str, &'a str>, h: &'h mut Hash
         Entry::Vacant(ve) => ve.insert(g.add_node(v.clone())),
         Entry::Occupied(ref oe) => oe.get(),
     };
-    println!("OR_INSERT {} -> {:?}", v, ix);
+    // println!("OR_INSERT {} -> {:?}", v, ix);
     return *ix;
 }
+
+// pub fn find_roots<'a, 'g>(g: &'g mut Graph<&'a str, &'a str>) -> Vec<NodeIndex<DefaultIx>> {
+//     let nf = NodeFiltered::from_fn(g, |ix| {
+//         g.edges_directed(ix, Incoming).next().is_none()
+//     });
+// }
 
 pub fn render(v: Vec<Syn>) {
     // println!("ok\n\n");
@@ -76,20 +85,22 @@ pub fn render(v: Vec<Syn>) {
         let mut v_nodes = HashMap::<&str, NodeIndex<DefaultIx>>::new();
         let mut h_nodes = HashMap::<&str, NodeIndex<DefaultIx>>::new();
 
+        let mut h_name = HashMap::new();
+        let mut h_styl = HashMap::new();
+        let mut h_acts = HashMap::new();
+        let mut h_sens = HashMap::new();
+
         for hint in res {
             match hint {
-                Fact::Fact(Ident("compact"), compacts) => {
-                    let mut h_acts = HashMap::new();
-                    let mut h_sens = HashMap::new();
-
-                    for item in compacts {
+                Fact::Fact(Ident(style), items) => {
+                    for item in items {
                         let item_ident = unwrap_atom(item).unwrap();
                         let v_ix = or_insert(&mut vert, &mut v_nodes, item_ident);
                         // let _h_ix = or_insert(&mut horz, &h_nodes, item_ident);
                         
                         let resolved_item = resolve(v.iter(), item).collect::<Vec<&Fact>>();
                         let name = as_string(&resolved_item, &name_query, item_ident.into());
-                        println!(r#"{}/{};"#, unwrap_atom(item).unwrap(), tikz_escape(&name));
+                        // println!(r#"{}/{};"#, unwrap_atom(item).unwrap(), tikz_escape(&name));
 
                         // TODO: need to loop here to resolve all the actuates/senses/hosts pairs, not just the first
                         let resolved_actuates = find_parent(v.iter(), &Ident("actuates"), to_ident(item)).next();
@@ -97,6 +108,9 @@ pub fn render(v: Vec<Syn>) {
                         let resolved_hosts = find_parent(v.iter(), &Ident("hosts"), to_ident(item)).next();
                         let action = as_string(&resolved_item, &action_query, unwrap_atom(item).unwrap().into());
                         let percept = as_string(&resolved_item, &percept_query, unwrap_atom(item).unwrap().into());
+
+                        h_styl.insert(item_ident, style);
+                        h_name.insert(item_ident, name);
 
                         h_acts.entry((resolved_actuates, resolved_hosts))
                             .or_insert(vec![])
@@ -121,105 +135,102 @@ pub fn render(v: Vec<Syn>) {
                             vert.add_edge(client_vix, v_ix, "rides");
                         }
                     }
-
-                    for ((src, tgt), mediators) in h_acts.iter() {
-                        if let (Some(s), Some(t)) = (src, tgt) {
-                            for (med, action) in mediators {
-                                println!(r#"({}) ->["{}"] ({});"#, s, tikz_escape(action), med);
-                                println!(r#"({}) -> ({});"#, med, t);
-                            }
-                        }
-                    }
-                    for ((src, tgt), mediators) in h_sens.iter() {
-                        if let (Some(s), Some(t)) = (src, tgt) {
-                            for (med, percept) in mediators {
-                                println!(r#"({}) <-["{}"] ({});"#, s, tikz_escape(percept), med);
-                                println!(r#"({}) <- ({});"#, med, t);
-                            }
-                        }
-                    }
-                },
-                Fact::Fact(Ident("coalesce"), coalesces) => {
-                    let mut h_acts = HashMap::new();
-                    let mut h_sens = HashMap::new();
-
-                    for item in coalesces {
-                        let item_ident = unwrap_atom(item).unwrap();
-                        let v_ix = or_insert(&mut vert, &mut v_nodes, item_ident);
-                        // let _h_ix = or_insert(&mut horz, &h_nodes, item_ident);
-                        
-                        let resolved_item = resolve(v.iter(), item).collect::<Vec<&Fact>>();
-
-                        // let src_query = Fact::Atom(Ident(""))  // "x where q \in x.actuates"
-                            // handle via unification? via a parser on facts??? via customs search routines?
-                            // via some kind of relational hackery?
-                        let resolved_actuates = find_parent(v.iter(), &Ident("actuates"), to_ident(item)).next();
-                        let resolved_senses = find_parent(v.iter(), &Ident("senses"), to_ident(item)).next();
-                        let resolved_hosts = find_parent(v.iter(), &Ident("hosts"), to_ident(item)).next();
-                        // println!("{:?}", resolved_src);
-
-                        let action = as_string(&resolved_item, &action_query, item_ident.into());
-                        let percept = as_string(&resolved_item, &percept_query, item_ident.into());
-
-                        h_acts.entry((resolved_actuates, resolved_hosts))
-                            .or_insert(vec![])
-                            .push(action);
-
-                        h_sens.entry((resolved_senses, resolved_hosts))
-                            .or_insert(vec![])
-                            .push(percept);
-
-                        if let Some(actuator) = resolved_actuates {
-                            let actuator_vix = or_insert(&mut vert, &mut v_nodes, actuator.0);
-                            vert.add_edge(actuator_vix, v_ix, "actuates");
-                        }
-
-                        if let Some(sensor) = resolved_senses {
-                            let sensor_vix = or_insert(&mut vert, &mut v_nodes, sensor.0);
-                            vert.add_edge(sensor_vix, v_ix, "senses");
-                        }
-
-                        if let Some(client) = resolved_hosts {
-                            let client_vix = or_insert(&mut vert, &mut v_nodes, client.0);
-                            vert.add_edge(v_ix, client_vix, "rides");
-                        }
-                    }
-
-                    for ((src, tgt), actions) in h_acts.iter() {
-                        if let (Some(s), Some(t)) = (src, tgt) {
-                            let rendered_actions = actions.as_slice().join("\\n");
-                            println!(r#"({}) ->["{}"] ({});"#, s, tikz_escape(&rendered_actions), t);
-                        }
-                    }
-
-                    for ((src, tgt), percepts) in h_sens.iter() {
-                        if let (Some(s), Some(t)) = (src, tgt) {
-                            let rendered_percepts = percepts.as_slice().join("\\n");
-                            println!(r#"({}) <-["{}"'] ({});"#, s, tikz_escape(&rendered_percepts), t);
-                        }
-                    }
                 },
                 _ => {},
             }
         }
 
-        println!("{}", indoc!(r#"
+        // find graph roots
+        // in a digraph, the roots are nodes with in-degree 0
+        let roots = vert.externals(Incoming).collect::<Vec<NodeIndex<DefaultIx>>>();
+        // println!("ROOTS {:?}", roots);
+
+        // let rvert = vert.filter_map(|_vx, vw| {Some(vw)}, |_ex, ew| {
+        //     match *ew {
+        //         "actuates" | "rides" => Some(ew),
+        //         _ => None,
+        //     }
+        // });
+
+        // println!("RVERT {:?}", rvert);
+
+        // println!("{}", "\n\n\n");
+        // println!("{:?}", Dot::new(&vert));
+        // println!("{}", "\n\n\n");
+
+        let paths = floyd_warshall(&vert, |_ex| { -1 }).unwrap();
+        let mut paths_from_roots = paths
+            .iter()
+            .filter_map(|((vx, wx), wgt)| {
+                if *wgt <= 0 && roots.contains(vx) {
+                    Some((*vx, *wx, -(*wgt)))
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>();
+
+        let mut paths_by_rank = BTreeMap::new();
+        for (vx, wx, wgt) in &paths_from_roots {
+            paths_by_rank
+                .entry(*wgt)
+                .or_insert(vec![])
+                .push((*vx, *wx));
+        }
+
+        // paths_from_roots
+        //     .sort_by_key(|(vx, wx, wgt)| { *wgt });
+        // println!("PATHS");
+        for (rank, paths) in paths_by_rank.iter() {
+            for (n, (vx, wx)) in paths.iter().enumerate() {
+                let _vl = vert.node_weight(*vx).unwrap();
+                let wl = vert.node_weight(*wx).unwrap();
+                let name = h_name.get(wl).unwrap();
+                let vpos = 1.5 * (*rank as f64);
+                let hpos = 3.5 * (n as f64);
+                // println!("  {} -> {}: {}", vl, wl, wgt);
+                println!(indoc!(r#"
+                    \node[minimum width = 2.5cm, fill=white, fill opacity=0.9, draw, text opacity=1.0]({}) at ({}, {}) {{{}}};"#), 
+                    wl, hpos, vpos, name);
+            }
+        }
+
+        for vx in vert.node_indices() {
+            let src = vert.node_weight(vx).unwrap();
+            // if let Some(&&"coalesced") = h_styl.get(src) {
+                let mut nbrs = vert.neighbors(vx).detach();
+                while let Some((ex, wx)) = nbrs.next(&vert) {
+                    let dst = vert.node_weight(wx).unwrap();
+                    let edge = vert.edge_weight(ex).unwrap();
+                    println!(indoc!(r#"
+                        \draw [-{{Latex[]}},postaction={{decorate}}] ($({}.south west)!\lc!({}.south east)$)        to[]  node[scale=0.8, anchor=north east, fill=white, fill opacity = 0.8, text opacity = 1.0, draw, ultra thin] {{{}}} ($({}.north west)!\lc!({}.north east)$);"#),
+                        dst, dst, edge, src, src    
+                    )
+                }
+            // }
+        }
+
+        let l = -3;
+        let r = 12;
+        let t = 7;
+        let b = -1;
+        println!(indoc!(r#"
             \scope[on background layer]
-            \draw[help lines,very thin,step=1] (-3.2,-4.2) grid (3.2,1.2);
-            \foreach \x in {-3,...,3} {
-            \foreach \y in {-4,...,1} {
+            \draw[help lines,very thin,step=1] ({}.2,{}.2) grid ({}.2,{}.2);
+            \foreach \x in {{{},...,{}}} {{
+            \foreach \y in {{{},...,{}}} {{
                 \draw [fill, black] (\x, \y) circle (0.5pt); 
-                \node[scale=0.5, anchor=south east] at (\x, \y) {\x,\y};
-            }
-            }
+                \node[scale=0.5, anchor=south east] at (\x, \y) {{\x,\y}};
+            }}
+            }}
             \endscope
 
-            }
-            \end{document}
-        "#));
+            }}
+            \end{{document}}
+        "#), l, b, r, t, l, r, b, t);
 
-        println!("{}", "\n\n\n");
-        println!("{:?}", Dot::new(&vert));
+        // println!("{}", "\n\n\n");
+        // println!("{:?}", Dot::new(&vert));
     }
     // use top-level "draw" fact to identify inline or top-level drawings to draw
     // resolve top-level drawings + use inline drawings to identify objects to draw to make particular drawings
