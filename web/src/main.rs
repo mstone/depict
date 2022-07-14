10760bacc2fbb09f9908d82ec10407ef9a76d21f
Michael Stone <m@mstone.info>
2022-07-14 18:08:11 -0400

    dioxus, web: factor out dom-based drawing code

--- web/src/main.rs
+++ web/src/main.rs
@@ -5,8 +5,9 @@ use std::{default::Default, panic::catch_unwind, io::BufWriter, borrow::Cow};
 use depict::{graph_drawing::{
     error::{Kind, Error, OrErrExt},
     layout::{Loc}, 
-    index::{LocSol, HopSol, VerticalRank, OriginalHorizontalRank}},
-};
+    index::{LocSol, HopSol, VerticalRank, OriginalHorizontalRank},
+    frontend::dom::{draw, Drawing, Label, Node},
+}};
 
 use dioxus::{prelude::*, core::to_owned};
 
@@ -134,226 +135,6 @@ unsafe extern "C" fn __toupper(_: __darwin_ct_rune_t) -> __darwin_ct_rune_t {
     todo!()
 }
 
-#[derive(Clone, Debug, PartialEq, PartialOrd)]
-pub struct Label {
-    pub text: String,
-    pub hpos: f64,
-    pub width: f64,
-    pub vpos: f64,
-}
-
-#[derive(Clone, Debug, PartialEq, PartialOrd)]
-pub enum Node {
-  Div { key: String, label: String, hpos: f64, vpos: f64, width: f64, loc: LocSol, estimated_width: f64 },
-  Svg { key: String, path: String, rel: String, label: Option<Label>, hops: Vec<HopSol>, estimated_width: (f64,f64) },
-}
-
-#[derive(Clone, Debug)]
-pub struct Drawing {
-    pub crossing_number: Option<usize>,
-    pub viewbox_width: f64,
-    pub nodes: Vec<Node>,
-}
-
-impl Default for Drawing {
-    fn default() -> Self {
-        Self { 
-            crossing_number: Default::default(), 
-            viewbox_width: 1024.0,
-            nodes: Default::default() 
-        }
-    }
-}
-
-fn draw(data: String) -> Result<Drawing, Error> {
-    let render_cell = depict::graph_drawing::frontend::render(Cow::Owned(data))?;
-    let depiction = render_cell.borrow_dependent();
-    
-    let rs = &depiction.geometry_solution.rs;
-    let ls = &depiction.geometry_solution.ls;
-    let ss = &depiction.geometry_solution.ss;
-    let ts = &depiction.geometry_solution.ts;
-    let sol_by_loc = &depiction.geometry_problem.sol_by_loc;
-    let loc_to_node = &depiction.layout_problem.loc_to_node;
-    let vert_node_labels = &depiction.vcg.vert_node_labels;
-    let vert_edge_labels = &depiction.vcg.vert_edge_labels;
-    let hops_by_edge = &depiction.layout_problem.hops_by_edge;
-    let sol_by_hop = &depiction.geometry_problem.sol_by_hop;
-    let width_by_loc = &depiction.geometry_problem.width_by_loc;
-    let width_by_hop = &depiction.geometry_problem.width_by_hop;
-    let crossing_number = depiction.layout_solution.crossing_number;
-    let condensed = &depiction.cvcg.condensed;
-
-    let height_scale = 80.0;
-    let vpad = 50.0;
-    let line_height = 20.0;
-
-    let mut texts = vec![];
-
-    let root_n = sol_by_loc[&(VerticalRank(0), OriginalHorizontalRank(0))];
-    let root_width = rs[root_n] - ls[root_n];
-
-    for (loc, node) in loc_to_node.iter() {
-        let (ovr, ohr) = loc;
-        if (*ovr, *ohr) == (VerticalRank(0), OriginalHorizontalRank(0)) { continue; }
-        let n = sol_by_loc[&(*ovr, *ohr)];
-
-        let lpos = ls[n];
-        let rpos = rs[n];
-
-        let vpos = height_scale * ((*ovr-1).0 as f64) + vpad + ts[*ovr] * line_height;
-        let width = (rpos - lpos).round();
-        let hpos = lpos.round();
-
-        if let Loc::Node(vl) = node {
-            let key = vl.to_string();
-            let label = vert_node_labels
-                .get(vl)
-                .or_err(Kind::KeyNotFoundError{key: vl.to_string()})?
-                .clone();
-            // if !label.is_screaming_snake_case() {
-            //     label = label.to_title_case();
-            // }
-            let estimated_width = width_by_loc[&(*ovr, *ohr)];
-            texts.push(Node::Div{key, label, hpos, vpos, width, loc: n, estimated_width});
-        }
-    }
-
-    let mut arrows = vec![];
-
-    for cer in condensed.edge_references() {
-        let mut prev_vwe = None;
-        for (m, (vl, wl, ew)) in cer.weight().iter().enumerate() {
-            if *vl == "root" { continue; }
-
-            if prev_vwe == Some((vl, wl, ew)) {
-                continue
-            } else {
-                prev_vwe = Some((vl, wl, ew))
-            }
-
-            let label_text = vert_edge_labels
-                .get(vl)
-                .and_then(|dsts| dsts
-                    .get(wl)
-                    .and_then(|rels| rels.get(ew)))
-                .map(|v| v.join("\n"));
-
-            let hops = &hops_by_edge[&(vl.clone(), wl.clone())];
-            // eprintln!("vl: {}, wl: {}, hops: {:?}", vl, wl, hops);
-
-            let offset = match ew { 
-                x if x == "actuates" => -10.0,
-                x if x == "actuator" => -10.0,
-                x if x == "senses" => 10.0,
-                x if x == "sensor" => 10.0,
-                _ => 0.0,
-            };
-
-            let mut path = vec![];
-            let mut label_hpos = None;
-            let mut label_width = None;
-            let mut label_vpos = None;
-            // use rand::Rng;
-            // let mut rng = rand::thread_rng();
-            let mut hn0 = vec![];
-            let mut estimated_width0 = None;
-
-            for (n, hop) in hops.iter().enumerate() {
-                let (lvl, (mhr, nhr)) = hop;
-                // let hn = sol_by_hop[&(*lvl+1, *nhr, vl.clone(), wl.clone())];
-                let hn = sol_by_hop[&(*lvl, *mhr, vl.clone(), wl.clone())];
-                let spos = ss[hn];
-                let hnd = sol_by_hop[&(*lvl+1, *nhr, vl.clone(), wl.clone())];
-                let sposd = ss[hnd];
-                let hpos = (spos + offset).round(); // + rng.gen_range(-0.1..0.1));
-                let hposd = (sposd + offset).round();
-                let vpos = ((*lvl-1).0 as f64) * height_scale + vpad + ts[*lvl] * line_height;
-                let mut vpos2 = (lvl.0 as f64) * height_scale + vpad + ts[(*lvl+1)] * line_height;
-
-                if n == 0 {
-                    hn0.push(hn);
-                }
-                hn0.push(hnd);
-                
-                if n == 0 {
-                    estimated_width0 = Some(width_by_hop[&(*lvl, *mhr, vl.clone(), wl.clone())]);
-                    let mut vpos = vpos;
-                    if *ew == "senses" {
-                        vpos += 33.0; // box height + arrow length
-                    } else {
-                        vpos += 26.0;
-                    }
-                    path.push(format!("M {hpos} {vpos}"));
-                }
-
-                if n == 0 {
-                    let n = sol_by_loc[&((*lvl+1), *nhr)];
-                    // let n = sol_by_loc[&((*lvl), *mhr)];
-                    label_hpos = Some(match ew {
-                        x if x == "senses" => {
-                            // ls[n]
-                            hposd
-                        },
-                        x if x == "actuates" => {
-                            // ls[n]
-                            hposd
-                        },
-                        _ => hposd
-                    });
-                    label_width = Some(match ew {
-                        x if x == "senses" => {
-                            // ls[n]
-                            rs[n] - sposd
-                        },
-                        x if x == "actuates" => {
-                            // ls[n]
-                            sposd - ls[n]
-                        },
-                        _ => rs[n] - ls[n]
-                    });
-                    label_vpos = Some(((*lvl-1).0 as f64) * height_scale + vpad + ts[*lvl] * line_height);
-                }
-
-                if n < hops.len() - 1 {
-                    vpos2 += 26.0;
-                }
-
-                if n == hops.len() - 1 && *ew == "actuates" { 
-                    vpos2 -= 7.0; // arrowhead length
-                }
-
-                path.push(format!("L {hposd} {vpos2}"));
-
-            }
-
-            let key = format!("{vl}_{wl}_{ew}_{m}");
-            let path = path.join(" ");
-
-            let mut label = None;
-
-            if let (Some(label_text), Some(label_hpos), Some(label_width), Some(label_vpos)) = (label_text, label_hpos, label_width, label_vpos) {
-                label = Some(Label{text: label_text, hpos: label_hpos, width: label_width, vpos: label_vpos})
-            }
-            arrows.push(Node::Svg{key, path, rel: ew.to_string(), label, hops: hn0, estimated_width: estimated_width0.unwrap()});
-        }
-    }
-
-    let nodes = texts
-        .into_iter()
-        .chain(arrows.into_iter())
-        .collect::<Vec<_>>();
-
-    event!(Level::TRACE, %root_width, ?nodes, "NODES");
-    // println!("NODES: {nodes:#?}");
-
-    Ok(Drawing{
-        crossing_number: Some(crossing_number), 
-        viewbox_width: root_width,
-        nodes
-    })
-}
-
 const PLACEHOLDER: &str = indoc!("
     person microwave food: open, start, stop / beep : heat
     person food: stir
@@ -400,12 +181,12 @@ pub fn as_data_svg(drawing: Drawing) -> String {
     nodes.sort_by(|a, b| a.partial_cmp(b).unwrap());
     for node in nodes {
         match node {
-            Node::Div{label, hpos, vpos, width, ..} => {
+            Node::Div{label, hpos, vpos, width, height, ..} => {
                     svg.append(Group::new()
                         .set("transform", format!("translate({hpos}, {vpos})"))
                         .add(Rectangle::new()
                             .set("width", width)
-                            .set("height", 26f64)
+                            .set("height", height)
                             .set("stroke", "black")
                             .set("fill", "none"))
                         .add(TextElt::new()
@@ -476,7 +257,7 @@ pub fn render<P>(cx: Scope<P>, drawing: Drawing)-> Option<VNode> {
     nodes.sort_by(|a, b| a.partial_cmp(b).unwrap());
     for node in nodes {
         match node {
-            Node::Div{key, label, hpos, vpos, width, ..} => {
+            Node::Div{key, label, hpos, vpos, width, height, z_index, ..} => {
                 children.push(cx.render(rsx! {
                     div {
                         key: "{key}",
@@ -484,13 +265,15 @@ pub fn render<P>(cx: Scope<P>, drawing: Drawing)-> Option<VNode> {
                         top: "{vpos}px",
                         left: "{hpos}px",
                         width: "{width}px",
+                        height: "{height}px",
+                        z_index: "{z_index}",
                         span {
                             "{label}"
                         }
                     }
                 }));
             },
-            Node::Svg{key, path, rel, label, ..} => {
+            Node::Svg{key, path, z_index, rel, label, ..} => {
                 let marker_id = if rel == "actuates" { "arrowhead" } else { "arrowheadrev" };
                 let marker_orient = if rel == "actuates" { "auto" } else { "auto-start-reverse" };
                 let stroke_dasharray = if rel == "fake" { "5 5" } else { "none" };
@@ -499,6 +282,7 @@ pub fn render<P>(cx: Scope<P>, drawing: Drawing)-> Option<VNode> {
                     div {
                         key: "{key}",
                         style: "position: absolute;",
+                        z_index: "{z_index}",
                         svg {
                             fill: "none",
                             stroke: "{stroke_color}",
